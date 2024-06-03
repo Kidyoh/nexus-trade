2733eb40c3519bf4f3eb4e2e4cafa44bf7ff1ebc
Kidyoh <Kidyoh789@gmail.com>
2024-06-03 11:28:37 +0300

    schema updated

--- src/pages/api/auth/register.js
+++ src/pages/api/auth/register.js
@@ -1,3 +1,26 @@
-import NextAuth from "next-auth/next";
-import Credentials from "next-auth/providers/credentials";
-import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import { prisma } from '../../../lib/prisma';
+import bcrypt from 'bcrypt';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'User already exists' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
